e414ccc89347c189da2a470a319ddf3f816d501b
Darren <darren.matis@gmail.com>
2019-02-09 14:28:14 -0800

    CSV output works correctly

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ Ensure that you have ruby installed
 Execute: `bundle install` to get all necessary gems
 
 ## To run
-`ruby ./app.rb`
+`ruby ./app.rb DIRECTORY_TO_SEARCH (optional)`
+
+## Results
+`output.csv`
--- app.rb
+++ app.rb
@@ -1,16 +1,53 @@
+require 'exifr/jpeg'
+require 'csv'
+
+def clear_output_csv
+  File.open('output.csv', 'w+') {}
+end
+
+# Input: absolute path to a directory to search for .jpg images
+# Output: Array containing filepaths to discovered images
+def get_image_paths(directory)
+  Dir["#{directory}/**/*.jpg"]
+end
+
+# Input: Array of image filepaths
+def extract_exif_from_images(images)
+  images.each { |image| extract_exif_from_image image }
+end
+
+# Input: absolute filename path to a .jpg image
+def extract_exif_from_image(image)
+  filename = File.basename(image)
+  latitude = ''
+  longitude = ''
+  object = EXIFR::JPEG.new(image)
+  unless object.gps.nil?
+    latitude = object.gps.latitude
+    longitude = object.gps.longitude
+  end
+  CSV.open('output.csv', 'a+') do |csv|
+    csv << [filename, latitude, longitude]
+  end
+end
+
 directory = Dir.pwd
-# puts directory
-puts ARGV.length
-if ARGV.length > 0
-    # Use input directory if it exists, otherwise use default
-    input_dir = ARGV[0]
-    if Dir.exist?(input_dir)
-        directory = input_dir
-    else
-        puts "Directory: \"#{input_dir}\" not found"
-        puts "Defaulting to: #{directory}"
-    end
+
+unless ARGV.empty?
+  # Use input directory if it exists, otherwise use default
+  input_dir = ARGV[0]
+  if Dir.exist?(input_dir)
+    directory = input_dir
+  else
+    puts "Directory: \"#{input_dir}\" not found"
+    puts "Defaulting to: #{directory}"
+  end
 end
 
 puts "Executing image scan in: #{directory}"
 
+clear_output_csv
+image_paths = get_image_paths(directory)
+extract_exif_from_images(image_paths)
+
+puts 'Script completed'
